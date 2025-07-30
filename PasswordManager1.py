c88ea8a3892c48d10e83a5241cc96105ded158ee
CDoggMoney <st21033@howick.school.nz>
2025-07-30 13:38:03 +1200

    Update PasswordManager1.py

--- PasswordManager1.py
+++ PasswordManager1.py
@@ -6,10 +6,16 @@ def Login(dict):
     try:
         if username == dict["Login"]["Username"] and password == dict["Login"]["Password"]:
             print("meow")
+            LoginFrame.pack_forget()
+            LoginedFrame.pack(fill="both", expand=True)
         else:
             print("woof woof")
     except ValueError:
         print("moo")
+
+def Return():
+    LoginedFrame.pack_forget()
+    LoginFrame.pack(fill="both", expand=True)
 
 PasswordDictionary = {
     "Login":{
@@ -34,12 +40,21 @@ UsernameEntry.grid(row = 1, column = 1, sticky = "nsew")
 PasswordLabel = Label(LoginFrame,text = "Enter Password: ", font = "Arial 10")
 PasswordLabel.grid(row = 2, column = 0)
 #entry to type password into
-PasswordEntry = Entry(LoginFrame, justify = CENTER, font = "Arial 15")
+PasswordEntry = Entry(LoginFrame, justify = CENTER, font = "Arial 15", show = "*")
 PasswordEntry.grid(row = 2, column = 1, sticky = "nsew")
 #button to confirm username and password
 LoginButton = Button(LoginFrame, text = "Login", width = 10, height = 2,
                           command = lambda: Login(PasswordDictionary), bg = "grey")
 LoginButton.grid(row = 3, column = 0, sticky = "nsew", padx = 5, pady = 5, columnspan = 2)
 
+
+#Frame which will appear upon successful login
+LoginedFrame = Frame(root)
+SuccessLabel = Label(LoginedFrame,text = "Login Successful", font = "Arial 15 bold")
+SuccessLabel.grid(row = 0, column = 0)
+#Back to login button
+BackButton = Button(LoginedFrame, text = "Back To Login", width = 10, height = 2,
+                          command = Return)
+BackButton.grid(row = 1, column = 0)
 
 root.mainloop()
