5f9e98434876f1936ca40a9cbc7cce804b61c060
CDoggMoney <st21033@howick.school.nz>
2025-08-26 14:13:15 +1200

    Update PasswordManagerV3.py

--- PasswordManagerV3.py
+++ PasswordManagerV3.py
@@ -54,7 +54,17 @@ def ConfirmPassword():
         "Email": Username, "Password": Password}
     }
     data.update(NewUserPass)
-    print(NewUserPass)
+
+def ConfirmRemoval():
+    Choice = RemoveChosen.get()
+    if Choice != "                                 ":
+        print(Choice)
+        data.pop(Choice, None)
+        IfLabel = Label(RemovePassFrame, font = "Arial 8", text = "Account Information Deleted Successfully")
+        IfLabel.grid(row = 5, column = 0)
+    else:
+        ElseLabel = Label(RemovePassFrame, font = "Arial 8", text = "You need to select something for the confirm button to work.")
+        ElseLabel.grid(row = 4, column = 0)
 
 def ViewPasswords():
     LoginedFrame.pack_forget()
@@ -69,10 +79,12 @@ def AddPassword():
     AddPassFrame.pack(fill="both", expand=True)
 
 def RemovePassword():
-    print("woof")
+    MenuFrame.pack_forget()
+    RemovePassFrame.pack(fill="both", expand=True)
 
 def ReturnToMenu():
     AddPassFrame.pack_forget()
+    RemovePassFrame.pack_forget()
     MenuFrame.pack(fill="both", expand=True)
 
 #Dictionary for the login component
@@ -195,34 +207,49 @@ MenuBackButton.grid(row = 2, column = 0, columnspan = 2)
 AddPassFrame = Frame(root)
 AddPassLabel = Label(AddPassFrame, text = "Add Password", font = "Arial 20 bold")
 AddPassLabel.grid(row = 0, column = 0, columnspan = 2)
-
 #Account name entry
 AddAccountLabel = Label(AddPassFrame, text = "What is this account for: ", font = "Arial 10")
 AddAccountLabel.grid(row = 1, column = 0)
 AddAccountEntry = Entry(AddPassFrame, justify = CENTER, font = "Arial 15")
 AddAccountEntry.grid(row = 1, column = 1, sticky = "nsew")
-
 #Label and Entry for Username or email
 AddUsernameLabel = Label(AddPassFrame, text = "Email/Username: ", font = "Arial 10")
 AddUsernameLabel.grid(row = 2, column = 0)
 AddUsernameEntry = Entry(AddPassFrame, justify = CENTER, font = "Arial 15")
 AddUsernameEntry.grid(row = 2, column = 1, sticky = "nsew")
-
 #Label and Entry for Password
 AddPasswordLabel = Label(AddPassFrame, text = "Password: ", font = "Arial 10")
 AddPasswordLabel.grid(row = 3, column = 0)
 AddPasswordEntry = Entry(AddPassFrame, justify = CENTER, font = "Arial 15")
 AddPasswordEntry.grid(row = 3, column = 1, sticky = "nsew")
-
 #Button to confirm username and password entry
 ConfirmPasswordButton = Button(AddPassFrame, text = "Confirm selections", width = 15, height = 2,
                                command = ConfirmPassword)
 ConfirmPasswordButton.grid(row = 4, column = 0, columnspan = 2)
-
 #Back Button
 AddBackButton = Button(AddPassFrame, text = "Go Back", width = 15, height = 2,
                                command = ReturnToMenu)
 AddBackButton.grid(row = 5, column = 0, columnspan = 2)
+
+
+#Frame which appears when remove password is selected
+RemovePassFrame = Frame(root)
+RemovePassLabel = Label(RemovePassFrame, text = "Remove Password", font = "Arial 20 bold")
+RemovePassLabel.grid(row = 0, column = 0)
+#drop down menu to select the account
+RemoveChoices = data
+RemoveDefault = "                                 "
+RemoveChosen = StringVar()
+RemoveOptions = ttk.OptionMenu(RemovePassFrame, RemoveChosen, RemoveDefault, *RemoveChoices)
+RemoveOptions.grid(row = 1, column = 0)
+#Button for confirming the account to remove
+ViewSelectButton = Button(RemovePassFrame, text = "Confirm", width = 10, height = 2,
+                          command = ConfirmRemoval)
+ViewSelectButton.grid(row = 2, column = 0)
+#Back button
+RemoveBackButton = Button(RemovePassFrame, text = "Go Back", width = 10, height = 2,
+                               command = ReturnToMenu)
+RemoveBackButton.grid(row = 3, column = 0, columnspan = 2)
 
 root.mainloop()
 with open("Passwords.json", "w") as f:
